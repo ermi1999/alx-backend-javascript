c298dc9ef777aa6d1580c956b753b7f2c0ab9775
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-24 17:16:57 +0300

    fixed task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+    .then((responses) => (
+      responses.map((res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
       }))
     ));
 }
