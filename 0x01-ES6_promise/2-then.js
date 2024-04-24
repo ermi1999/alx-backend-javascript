971b18c0ec1a58ae614f6192331cf3548a202b9e
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-24 08:24:56 +0300

    multiple successful promises

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => Error())
+    .finally(console.log('Got a response from the API'));
+}
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+export default function handleProfileSignup() {
+  uploadPhoto()
+    .then((photo) => {
+      createUser().then((user) => {
+        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+      });
+    })
+    .catch((err) => console.log('Signup system offline'));
+}
