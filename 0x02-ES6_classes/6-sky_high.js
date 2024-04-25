e7d2ac03e20731fa26c8115946fbfa96d163a339
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-25 20:05:06 +0300

    sky high building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  set floors(newFloor) {
+    if (typeof newFloor !== 'number') {
+      throw new TypeError('Floor must be a number');
+    }
+    this._floors = newFloor;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
