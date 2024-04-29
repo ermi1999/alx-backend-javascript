c8ced001a40d31949de40489e3fc3b7d6c3a5501
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-29 22:55:13 +0300

    update grade by city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,20 @@
-export default function updateStudentGradeByCity()
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      let studentGrade = 'N/A';
+
+      const newGrade = newGrades.filter(
+        (grade) => grade.studentId === student.id,
+      );
+
+      if (newGrade.length > 0) {
+        studentGrade = newGrade[0].grade;
+      }
+
+      // eslint-disable-next-line no-param-reassign
+      student.grade = studentGrade;
+
+      return student;
+    });
+}
