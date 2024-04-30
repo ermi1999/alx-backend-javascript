fcf0a45f7d2bb56d135870bb078fd5db6567c67a
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-30 13:22:54 +0300

    weak map fixed

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) > 4) {
+  if (weakMap.get(endpoint) >= 4) {
     throw new Error('Endpoint load is high');
   }
   if (!weakMap.get(endpoint)) {
