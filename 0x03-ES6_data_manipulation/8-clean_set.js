d3af4098ebf446c5c38c0d17b733e53ec54c60ae
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-30 13:41:37 +0300

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
+  if (startString === '' || typeof startString !== 'string') {
     return '';
   }
-  let result = '';
+  const result = [];
   for (const value of set) {
     if (value.startsWith(startString)) {
-      result += `${value.slice(startString.length)}-`;
+      result.push(value.slice(startString.length));
     }
   }
-  result = result.slice(0, result.length - 1);
-
-  return result;
+  return result.join('-');
 }
