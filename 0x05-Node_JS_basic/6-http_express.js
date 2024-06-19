364f0c5ef51891b7a8928852cd93d9f821de1237
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-19 22:21:31 +0300

    express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
